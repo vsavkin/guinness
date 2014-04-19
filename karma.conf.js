f58970192a42a6d87ef1a659b5e21296b6d2d86f
Victor Savkin <vic.savkin@gmail.com>
2014-04-19 15:08:45 -0400

    adds karma config

---
+++ karma.conf.js
@@ -0,0 +1,25 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '.',
+    frameworks: ['dart-unittest'],
+
+    files: [
+      'test/jasmine_test.dart',
+      {pattern: '**/*.dart', watched: true, included: false, served: true}
+    ],
+
+    exclude: [
+    ],
+
+    autoWatch: true,
+    captureTimeout: 20000,
+    browserNoActivityTimeout: 300000,
+
+    plugins: [
+      'karma-dart',
+      'karma-chrome-launcher'
+    ],
+
+    browsers: []
+  });
+};
