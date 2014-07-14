37dbc868f076dbdfb7585530475ed02ff71f5a4e
Victor Savkin <vic.savkin@gmail.com>
2014-07-13 22:10:17 -0400

    Make the syntax more flexible by allowing passing any object as a name (e.g., `describe(MyClass, (){}`).

--- lib/src/common/syntax.dart
+++ lib/src/common/syntax.dart
@@ -6,24 +6,24 @@ void beforeEach(Function fn, {int priority:0}) =>
 void afterEach(Function fn, {int priority:0}) =>
     guinness._context.addAfterEach(fn, priority: priority);
 
-void it(String name, [Function fn]) =>
-    guinness._context.addIt(name, fn, excluded: false, exclusive: false);
+void it(name, [Function fn]) =>
+    guinness._context.addIt(name.toString(), fn, excluded: false, exclusive: false);
 
-void xit(String name, [Function fn]) =>
-    guinness._context.addIt(name, fn, excluded: true, exclusive: false);
+void xit(name, [Function fn]) =>
+    guinness._context.addIt(name.toString(), fn, excluded: true, exclusive: false);
 
-void iit(String name, [Function fn]) =>
-    guinness._context.addIt(name, fn, excluded: false, exclusive: true);
+void iit(name, [Function fn]) =>
+    guinness._context.addIt(name.toString(), fn, excluded: false, exclusive: true);
 
 
-void describe(String name, [Function fn]) =>
-    guinness._context.addDescribe(name, fn, excluded: false, exclusive: false);
+void describe(name, [Function fn]) =>
+    guinness._context.addDescribe(name.toString(), fn, excluded: false, exclusive: false);
 
-void xdescribe(String name, [Function fn]) =>
-    guinness._context.addDescribe(name, fn, excluded: true, exclusive: false);
+void xdescribe(name, [Function fn]) =>
+    guinness._context.addDescribe(name.toString(), fn, excluded: true, exclusive: false);
 
-void ddescribe(String name, [Function fn]) =>
-    guinness._context.addDescribe(name, fn, excluded: false, exclusive: true);
+void ddescribe(name, [Function fn]) =>
+    guinness._context.addDescribe(name.toString(), fn, excluded: false, exclusive: true);
 
 Expect expect(actual, [matcher]) {
   final expect = new Expect(actual);
